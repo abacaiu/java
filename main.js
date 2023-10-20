881ff5996a82a6cc04e30f747bc978e3c6686083
pedro <106682076+abacaiu@users.noreply.github.com>
2023-10-20 09:36:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio){ 
+    document.querySelector(idElementoAudio).play()
+} 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const 
+}
