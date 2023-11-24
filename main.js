4fda64f3369973e5d15a6cf51c2af0e2b1f9a9be
pedro <106682076+abacaiu@users.noreply.github.com>
2023-11-24 07:52:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
  function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+
+    const elemento = document.querySelector(idElementoAudio).play();
+ 
+    if (elemento && elemento.localName === 'audio') {
+         elemento.play();
+       
+    }
+    else {
+        //alert('Elemento ou seletor não encotrado');
+        console.log('Elemento ou seletor não encontrado!')
+    }
+
  }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+ const listaDeTeclas = document.querySelectorAll('.tecla')
 
-//para
-for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+ //para
+ for (let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`; //template string
-
-
-    tecla.onclick = function () {
-       tocaSom(idAudio) 
-    }
+    const intrumento = tecla.classList(1)
+ }
   
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa')
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
